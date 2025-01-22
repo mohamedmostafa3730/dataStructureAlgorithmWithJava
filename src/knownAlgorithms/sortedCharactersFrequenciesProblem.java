7421c59093f6395a8dd3acae18a19fb8c3c21a44
Mohamed Mostafa <mohamedmostafa3730@gmail.com>
2025-01-22 15:03:44 +0200

    sorted Characters Frequencies => HashMap|UTF-8

--- src/knownAlgorithms/sortedCharactersFrequenciesProblem.java
+++ src/knownAlgorithms/sortedCharactersFrequenciesProblem.java
@@ -1,10 +1,14 @@
 package knownAlgorithms;
 
+import java.util.HashMap;
+
 public class sortedCharactersFrequenciesProblem {
 
     public static void main(String[] args) {
         String message = "hello world";
         ASCIIMethod(message);
+        System.out.println("==========================");
+        hashMethod(message);
 
     }
 
@@ -23,6 +27,20 @@ public class sortedCharactersFrequenciesProblem {
                 System.out.println(freq[i]);
             }
 
+        }
+
+    }
+
+    public static void hashMethod(String message) {
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+
+        for (int i = 0; i < message.length(); i++) {
+            char currentChar = message.charAt(i);
+            freqMap.put(currentChar, freqMap.getOrDefault(currentChar, 0) + 1);
+        }
+        for (char c : freqMap.keySet()) {
+            System.out.print(c + " ");
+            System.out.println(freqMap.get(c));
         }
 
     }

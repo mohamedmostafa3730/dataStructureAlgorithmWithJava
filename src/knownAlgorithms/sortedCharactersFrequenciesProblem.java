de2cfb60f9ecc820c2f08e4a97b58d249a6d6f50
Mohamed Mostafa <mohamedmostafa3730@gmail.com>
2025-01-22 16:36:43 +0200

    sorted Characters Frequencies =>HashMab/marge Sort

--- src/knownAlgorithms/sortedCharactersFrequenciesProblem.java
+++ src/knownAlgorithms/sortedCharactersFrequenciesProblem.java
@@ -1,6 +1,7 @@
 package knownAlgorithms;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class sortedCharactersFrequenciesProblem {
 
@@ -41,6 +42,78 @@ public class sortedCharactersFrequenciesProblem {
         for (char c : freqMap.keySet()) {
             System.out.print(c + " ");
             System.out.println(freqMap.get(c));
+        }
+
+    }
+
+    public static int[][] sortWith2DArray(HashMap<Character, Integer> freqMap) {
+        int[][] sort2DArray = new int[freqMap.size()][2];
+        int i = 0;
+        for (Map.Entry<Character, Integer> dataMap : freqMap.entrySet()) {
+            sort2DArray[i][0] = dataMap.getKey();
+            sort2DArray[i][1] = dataMap.getValue();
+            i++;
+        }
+        return sort2DArray;
+    }
+
+    // marge Sort Algorithm
+    public static int[][] sort(int[][] toSort2DArray, int start, int end) {
+        if (end <= start) {
+            return toSort2DArray;
+        }
+        int midPoint = (end + start) / 2;
+        sort(toSort2DArray, start, midPoint);
+        sort(toSort2DArray, midPoint + 1, end);
+        marge(toSort2DArray, start, midPoint, end);
+        return toSort2DArray;
+
+    }
+
+    public static void marge(int[][] toSort2DArray, int start, int midPoint, int end) {
+        int i, j, k;
+        int lengthOfLeftArray = midPoint - start + 1;
+        int lengthOfRightArray = end - midPoint;
+        int[][] LeftArray = new int[lengthOfLeftArray][2];
+        int[][] RightArray = new int[lengthOfRightArray][2];
+        for (i = 0; i < LeftArray.length; i++) {
+            LeftArray[i][0] = toSort2DArray[start + i][0];
+            LeftArray[i][1] = toSort2DArray[start + i][1];
+        }
+        for (j = 0; j < RightArray.length; j++) {
+            RightArray[j][0] = toSort2DArray[midPoint + 1 + j][0];
+            RightArray[j][1] = toSort2DArray[midPoint + 1 + j][1];
+        }
+        j = 0;
+        i = 0;
+        k = start;
+
+        while (i < lengthOfLeftArray && i < lengthOfRightArray) {
+            if (LeftArray[i][1] <= RightArray[j][1]) {
+                toSort2DArray[k][0] = LeftArray[i][0];
+                toSort2DArray[k][1] = LeftArray[i][1];
+                i++;
+            } else {
+                toSort2DArray[k][0] = RightArray[j][0];
+                toSort2DArray[k][1] = RightArray[j][1];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < lengthOfLeftArray) {
+
+            toSort2DArray[k][0] = LeftArray[i][0];
+            toSort2DArray[k][1] = LeftArray[i][1];
+            i++;
+            k++;
+        }
+
+        while (j < lengthOfRightArray) {
+            toSort2DArray[k][0] = RightArray[j][0];
+            toSort2DArray[k][1] = RightArray[j][1];
+            j++;
+            k++;
         }
 
     }
